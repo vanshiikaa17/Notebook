2e28f1761e2cf514d1820d74858cf31ab600675a
17-vanshi <vanshikamohindra.vani@gmail.com>
2024-05-11 08:47:25 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./Components/Navbar";
+import { Home } from "./Components/Home";
+
+import NoteState from "./Context/notes/NoteState";
+import { Login } from "./Components/Login";
+import { Signup } from "./Components/Signup";
+import { Alert } from "./Components/Alert";
+import { useState } from "react";
+import { LandingPage } from "./Components/LandingPage";
 function App() {
+  const [alert, setAlert] = useState(null); 
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar showAlert={showAlert}/>
+          <Alert alert={alert}/>
+
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home showAlert={showAlert}/>}/>
+              <Route exact path="/home" element={<LandingPage/>} />
+              <Route exact path="/login" element={<Login showAlert={showAlert}/>} />
+              <Route exact path="/signup" element={<Signup showAlert={showAlert}/>}/>
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
